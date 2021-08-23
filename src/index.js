f331acdce18635066c379081896eb7f667602072
shmel-kai <al88.korotkevich@gmail.com>
2021-08-24 00:56:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) { 
+    const arr = expr.split("");
+    const charsNumber = arr.length / 10;
+    let resultString = '';
+
+    for (let i = 0; i < charsNumber; i++) {
+        const one = arr[0 + i * 10] + arr[1 + i * 10];
+        const two = arr[2 + i * 10] + arr[3 + i * 10];
+        const three = arr[4 + i * 10] + arr[5 + i * 10];
+        const four = arr[6 + i * 10] + arr[7 + i * 10];
+        const five = arr[8 + i * 10] + arr[9 + i * 10];
+    
+        const map = {
+            '10': '.',
+            '11': '-',
+            '00': '',
+        };
+    
+        const resultsString = map[one] + map[two] + map[three] + map[four] + map[five];
+
+        if (one === '**') {
+            resultString += ' ';
+        } else {
+            resultString += MORSE_TABLE[resultsString];
+        }
+    }
+
+    return resultString;
 }
 
 module.exports = {
